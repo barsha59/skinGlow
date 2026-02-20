45c8ae0e879306a5b738fd77c2ce3f2d78dd1bd9
barsha karna <barsha@kali.barsha.com>
2026-02-20 15:53:54 +0545

    Update API_URL for EC2 deployemnt

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,5 @@
 // frontend/src/config.js
-const API_URL =
-  process.env.REACT_APP_API_URL || "http://backend:5002";  // <- use 'backend' here
+const API_URL = "http://100.30.70.54:5002"; // replaced with EC2 Elastic IP
 const STRIPE_PUBLISHABLE_KEY =
   "pk_test_51Sl1TM2YdULA0kvG2JENo4pXukqssFkAHNfEYoeqzdGrkIxBLJh8YirmBdusf6yaCJxQXWq1W7usBupkDNupbPHf00AVabIsFL";
 
